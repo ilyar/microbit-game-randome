681f4c4263bcabc85a3c66ce7d9e1d617fe651f9
Ilyar <761285+ilyar@users.noreply.github.com>
2022-11-12 12:19:17 +0100

    first release

--- main.ts
+++ main.ts
@@ -1,12 +1,41 @@
-function START () {
-    ALICE = -1
-    BOB = -1
-    radio.sendNumber(-1)
-}
-let BOB = 0
-let ALICE = 0
-radio.setGroup(1)
-START()
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    BOB += receivedNumber
+    GAME()
 })
+input.onButtonPressed(Button.A, function () {
+    ALICE = randint(1, 10)
+    radio.sendNumber(ALICE)
+    GAME()
+})
+function START () {
+    ALICE = 0
+    BOB = 0
+}
+function GAME () {
+    if (ALICE == 0) {
+        basic.showString("?")
+    } else if (BOB == 0) {
+        basic.showLeds(`
+            # # # # #
+            . # # # .
+            . . # . .
+            . # # # .
+            # # # # #
+            `)
+    } else {
+        if (ALICE > BOB) {
+            basic.showIcon(IconNames.Happy)
+        } else if (ALICE < BOB) {
+            basic.showIcon(IconNames.Sad)
+        } else {
+            basic.showIcon(IconNames.Asleep)
+        }
+        START()
+    }
+}
+let ALICE = 0
+let BOB = 0
+radio.setGroup(1)
+radio.sendNumber(0)
+START()
+GAME()
